e30b39a8b59144d066641cc28f167356b9ec6732
arnnavk <arnavbansal05@gmail.com>
2021-06-04 19:02:23 +0530

    Final Website

--- script2.js
+++ script2.js
@@ -44,6 +44,78 @@ function change(){
 
   else if(image_tracker=='5')
   {
+    image.src='6.png';
+    image_tracker='6';
+  }
+
+  else if(image_tracker=='6')
+  {
+    image.src='7.png';
+    image_tracker='7';
+  }
+
+  else if(image_tracker=='7')
+  {
+    image.src='8.png';
+    image_tracker='8';
+  }
+
+  else if(image_tracker=='8')
+  {
+    image.src='9.png';
+    image_tracker='9';
+  }
+
+  else if(image_tracker=='9')
+  {
+    image.src='10.png';
+    image_tracker='10';
+  }
+
+  else if(image_tracker=='10')
+  {
+    image.src='11.png';
+    image_tracker='11';
+  }
+
+  else if(image_tracker=='11')
+  {
+    image.src='12.png';
+    image_tracker='12';
+  }
+
+  else if(image_tracker=='12')
+  {
+    image.src='13.png';
+    image_tracker='13';
+  }
+
+  else if(image_tracker=='13')
+  {
+    image.src='14.png';
+    image_tracker='14';
+  }
+
+  else if(image_tracker=='14')
+  {
+    image.src='15.png';
+    image_tracker='15';
+  }
+
+  else if(image_tracker=='15')
+  {
+    image.src='16.png';
+    image_tracker='16';
+  }
+
+  else if(image_tracker=='16')
+  {
+    image.src='17.png';
+    image_tracker='17';
+  }
+
+  else if(image_tracker=='17')
+  {
     image.src='1.png';
     image_tracker='1';
   }
